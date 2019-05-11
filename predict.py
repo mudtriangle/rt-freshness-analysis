549a3e03e7f38002f04d47610c33d8f78d5dd3e9
Marcelo Sandoval-Castañeda <43766404+mudtriangle@users.noreply.github.com>
2019-05-11 11:35:10 +0400

    Added comments, reduced verbosity

--- predict.py
+++ predict.py
@@ -1,13 +1,16 @@
+# External libraries
 import torch
 import pandas as pd
-import numpy as np
 
+# Internal libraries
 import lstm_sentiment as lstm
 import data_utils as du
 import string_processing as sp
 
+# Constants
 DROP_PROB = 0.5
 
+# Values necessary to load the network.
 output_size = 1
 embedding_dim = 400
 hidden_dim = 256
@@ -16,20 +19,23 @@ vocab = pd.read_csv("vocabulary.txt", names=['ind', 'word'], encoding='iso-8859-
 vocab = pd.Series(vocab['ind'].values, index=vocab['word']).to_dict()
 vocab_size = du.get_vocab_size("vocabulary.txt")
 
+# Load the network.
 network = lstm.LSTMSentiment(vocab_size, output_size, embedding_dim, hidden_dim, num_layers, DROP_PROB)
 network.load_state_dict(torch.load('model'))
 network.eval()
 
+# Get user input.
 user_sentence = input("Enter a review: ")
+
+# Process user input and convert it to tokens.
 user_sentence = sp.normalize(user_sentence)
 user_sentence = sp.tokenize(user_sentence)
 user_sentence = sp.get_numbers(user_sentence, vocab)
 user_sentence = sp.padding(user_sentence, 30)
 
+# Predict and output results.
 output, h = network(torch.LongTensor([user_sentence]), network.init_hidden(1))
 pred = torch.round(output.squeeze())
-print('Prediction value, pre-rounding: {:.6f}'.format(output.item()))
-
 if pred.item() == 1:
     print("Fresh")
 else:
